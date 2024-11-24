8f663b9a34df65ce60dad0a4a81edf2e70c2de60
Natalia Kiełbasa <natalia.kielbasa2002@gmail.com>
2024-11-24 22:45:55 +0100

    ruby - search products

--- README.md
+++ README.md
@@ -21,7 +21,7 @@
 
 ## RUBY - Crawler (Empik)
 - [x] 3.0 Należy pobrać podstawowe dane o produktach (tytuł, cena), dowolna kategoria
-- [ ] 3.5 Należy pobrać podstawowe dane o produktach wg słów kluczowych
+- [x] 3.5 Należy pobrać podstawowe dane o produktach wg słów kluczowych
 - [ ] 4.0 Należy rozszerzyć dane o produktach o dane szczegółowe widoczne tylko na podstronie o produkcie
 - [ ] 4.5 Należy zapisać linki do produktów
 - [ ] 5.0 Dane należy zapisać w bazie danych np. SQLite via Sequel
--- Ruby/crawler.rb
+++ Ruby/crawler.rb
@@ -8,14 +8,23 @@ Product = Struct.new(:title, :price) do
     end
 end
 
-url = 'https://www.empik.com/szukaj/produkt?q=ksiazka'
-doc = Nokogiri::HTML(URI.open(url))
-
-products = []
-doc.css('.search-list-item').each do |product|
-    title = product.css('.ta-product-title').text.strip
-    price = product.css('.ta-price-tile[content]').text.strip
-    products.append(Product.new(title, price))
+def get_page(search_query)
+    url = "https://www.empik.com/szukaj/produkt?q=#{search_query}"
+    doc = Nokogiri::HTML(URI.open(url))
+    return doc
 end
 
+def search_products(doc)
+    products = []
+    doc.css('.search-list-item').each do |product|
+        title = product.css('.ta-product-title').text.strip
+        price = product.css('.ta-price-tile[content]').text.strip
+        products.append(Product.new(title, price))
+    end
+    return products
+end
+
+search_query = ARGV.join().gsub(' ', "%20")
+page = get_page(search_query)
+products = search_products(page)
 puts JSON.pretty_generate(products)
