1390a1c9d15d07f79e4884112352cfd1a26d8a64
Natalia Kiełbasa <natalia.kielbasa2002@gmail.com>
2024-11-25 19:47:13 +0100

    ruby - add productts to database

--- README.md
+++ README.md
@@ -22,9 +22,9 @@
 ## RUBY - Crawler (Empik)
 - [x] 3.0 Należy pobrać podstawowe dane o produktach (tytuł, cena), dowolna kategoria
 - [x] 3.5 Należy pobrać podstawowe dane o produktach wg słów kluczowych
-- [ ] 4.0 Należy rozszerzyć dane o produktach o dane szczegółowe widoczne tylko na podstronie o produkcie
+- [x] 4.0 Należy rozszerzyć dane o produktach o dane szczegółowe widoczne tylko na podstronie o produkcie
 - [x] 4.5 Należy zapisać linki do produktów
-- [ ] 5.0 Dane należy zapisać w bazie danych np. SQLite via Sequel
+- [x] 5.0 Dane należy zapisać w bazie danych np. SQLite via Sequel
 
 ## PYTHON - Czatbot
 - [ ] 3.0 A chatbot with the trained ability to handle at least 3 ways to phrase those intents.
--- Ruby/crawler.rb
+++ Ruby/crawler.rb
@@ -1,12 +1,24 @@
 require 'nokogiri'
 require 'open-uri'
 require 'json'
+require 'sequel'
 
-# Dodajemy info --> link, kategorie (1,2,3,...), ID, Ocena, Ilosc ocen
+DB = Sequel.sqlite('products.db')
 
-Product = Struct.new(:title, :price, :ID, :rating, :rating_count, :categories, :link) do
+DB.create_table? :products do
+    primary_key :id
+    String :title
+    String :price
+    String :product_id
+    Float :rating
+    Integer :rating_count
+    String :categories
+    String :link
+end
+
+class Product < Sequel::Model(:products)
     def to_json(*options)
-      to_h.to_json(*options)
+        to_h.to_json(*options)
     end
 end
 
@@ -19,9 +31,6 @@ end
 def get_product_details(url)
     begin
         doc = Nokogiri::HTML(URI.open(url))
-        File.open("hebe_search_results.html", "w") do |file|
-            file.write(doc.to_html)
-        end
 
         meta_tag = doc.at_css('meta[property="product:category_tree"]')
         if meta_tag
@@ -50,7 +59,16 @@ def search_products(doc)
         price = product.css('.ta-price-tile[content]').text.strip
         link = base_url + product.at_css('a.seoTitle')['href'].strip
         product_id, rating_score, rating_count, categories = get_product_details(link)
-        products.append(Product.new(title, price, product_id, rating_score, rating_count, categories, link))
+
+        products << Product.new(
+        title: title,
+        price: price,
+        product_id: product_id,
+        rating: rating_score,
+        rating_count: rating_count,
+        categories: categories,
+        link: link
+      )
     end
     return products
 end
@@ -58,4 +76,5 @@ end
 search_query = ARGV.join().gsub(' ', "%20")
 page = get_page(search_query)
 products = search_products(page)
-puts JSON.pretty_generate(products)
+products.each(&:save)
+puts "#{products.length} products saved to database."
