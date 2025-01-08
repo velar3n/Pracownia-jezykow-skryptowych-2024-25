c21bdabb0f41a353db9f4c7cf0f69e5b24d75a45
Natalia K <natalia.kielbasa2002@gmail.com>
2025-01-08 13:24:34 +0100

    js - added more castle parts

--- JS/minecraft_zamek.js
+++ JS/minecraft_zamek.js
@@ -1,6 +1,7 @@
 let wallHeight = 0
 let windowHeight = 0
 let wallLength = 0
+let moatDepth = 0
 function buildFloor () {
     builder.mark()
     for (let index = 0; index < 19; index++) {
@@ -26,7 +27,6 @@ function buildWalls () {
     builder.move(DOWN, 1)
     builder.move(BACK, 4)
     builder.move(LEFT, 2)
-    buildFloor()
 }
 function buildTower () {
     wallHeight = 15
@@ -57,6 +57,26 @@ function buildTower () {
     builder.turn(RIGHT_TURN)
     builder.turn(RIGHT_TURN)
 }
+function buildBridge () {
+    builder.move(BACK, 26)
+    builder.mark()
+    for (let index = 0; index < 4; index++) {
+        builder.move(BACK, 5)
+        builder.move(LEFT, 1)
+        builder.move(FORWARD, 5)
+        builder.move(LEFT, 1)
+    }
+    builder.move(BACK, 5)
+    builder.fill(PLANKS_DARK_OAK)
+    builder.move(UP, 1)
+    builder.mark()
+    builder.move(RIGHT, 8)
+    builder.fill(DARK_OAK_FENCE)
+    builder.move(FORWARD, 5)
+    builder.mark()
+    builder.move(LEFT, 8)
+    builder.fill(DARK_OAK_FENCE)
+}
 function buildWall () {
     wallLength = 30
     wallHeight = 10
@@ -77,6 +97,67 @@ function buildWall () {
     builder.raiseWall(TUFF_WALL, 1)
     builder.move(DOWN, wallHeight)
 }
+function buildMoat () {
+    moatDepth = 6
+    builder.move(RIGHT, 1)
+    builder.move(FORWARD, 1)
+    builder.move(DOWN, 5)
+    builder.mark()
+    for (let index = 0; index < 4; index++) {
+        builder.turn(LEFT_TURN)
+        builder.move(FORWARD, 40)
+    }
+    builder.raiseWall(COBBLESTONE, moatDepth)
+    for (let indeks = 0; indeks <= 6; indeks++) {
+        builder.move(FORWARD, 1)
+        builder.move(RIGHT, 1)
+        for (let index = 0; index < 4; index++) {
+            builder.mark()
+            builder.turn(LEFT_TURN)
+            builder.move(FORWARD, 42)
+            builder.move(FORWARD, indeks)
+            builder.move(FORWARD, indeks)
+            builder.tracePath(COBBLESTONE)
+        }
+        builder.move(UP, 4)
+        for (let index = 0; index < 4; index++) {
+            builder.mark()
+            builder.turn(LEFT_TURN)
+            builder.move(FORWARD, 42)
+            builder.move(FORWARD, indeks)
+            builder.move(FORWARD, indeks)
+            builder.tracePath(WATER)
+        }
+        builder.move(DOWN, 4)
+    }
+    builder.move(FORWARD, 1)
+    builder.move(RIGHT, 1)
+    builder.mark()
+    for (let index = 0; index < 4; index++) {
+        builder.turn(LEFT_TURN)
+        builder.move(FORWARD, 56)
+    }
+    builder.raiseWall(COBBLESTONE, moatDepth)
+    builder.move(UP, 6)
+}
 player.onChat("buildCastle", function () {
     buildWalls()
+    buildFloor()
+    buildMoat()
+    buildBridge()
+    buildGate()
 })
+function buildGate () {
+    builder.move(LEFT, 1)
+    builder.mark()
+    builder.move(BACK, 5)
+    builder.move(LEFT, 1)
+    builder.move(FORWARD, 5)
+    builder.move(LEFT, 1)
+    builder.move(BACK, 5)
+    builder.raiseWall(AIR, 7)
+    builder.move(RIGHT, 1)
+    builder.mark()
+    builder.move(FORWARD, 5)
+    builder.raiseWall(MANGROVE_FENCE, 7)
+}
